0873692ac9102f2fc9c1b51eb9ca6970613e7f70
J.D <shaefferjd@gmail.com>
2019-06-07 11:45:48 -0400

    adds the loop structure

--- fight.py
+++ fight.py
@@ -15,15 +15,40 @@ class enemy():
         pow = randint(0, self.level)
         # heal = randint(0, self.level // 2)
 
-# start game
-start = input("You're in a boxing ring. There's a guy in the opposite corner. Fight him? (y/n)")
-if input == "y":
-    print("DING DING DING")
-    sleep(1)
-    print("FIGHT!")
-elif input == "n":
-    print("WHAT?!")
-    sleep(1)
-    print("The crowd throws tomatoes at you.")
-    sys.exit()
-else:
+# flags
+start = True
+fight = False
+
+# game
+enemy = enemy(10,10)
+guy = player(10,10,0)
+print("You're in a boxing ring. There's a guy in the opposite corner. Fight him? (y/n)")
+
+while start is True:
+    x = input("> ") # x is generic var for all input string
+    if x is "y":
+        print("DING DING DING")
+        sleep(1)
+        print("FIGHT!")
+        sleep(1)
+        print("The challenger approaches. Type help for a tutorial.")
+        start = False
+        fight = True
+    elif x is "n":
+        print("WHAT?!")
+        sleep(1)
+        print("The crowd throws tomatoes at you.")
+        sleep(1)
+        print("THE END")
+        exit()
+    else:
+        print("Hm? Speak up son.\n")
+
+while fight is True:
+    if guy.hp == 0:
+        print("You fall to the ground.")
+        sleep(1)
+        print("GAME OVER")
+        exit()
+    else:
+        x = input("> ")
