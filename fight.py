4db16b3c58decaf3fb0e3f596a4ed6cd4b425aba
J.D <shaefferjd@gmail.com>
2019-06-07 15:32:12 -0400

    program dies when you die

--- fight.py
+++ fight.py
@@ -1,6 +1,7 @@
 from random import randint
 from time import sleep
 import threading
+import os
 
 class player():
     def __init__(self, level, hp, xp):
@@ -13,13 +14,15 @@ class enemy():
         self.level = level
         self.hp = hp
     def punch(self, player):
-        while player.hp != 0:
-            print("Your opponent throws a punch.") # block left/right eventually
-            pow = randint(0, 10) # will change to level based value
+        while player.hp > 0:
+            print("Your opponent throws a punch.\n") # block left/right eventually
+            pow = randint(0, 8) # will change to level based value
             player.hp = player.hp - pow
-            sleep(.5) 
-            print("YOUR HP:", player.hp)
             sleep(2) # will change to level based value
+        print("You fall to the ground.")
+        sleep(1)
+        print("GAME OVER")
+        os._exit(1) # kills the whole program (thread and main)
 
 if __name__ == "__main__":
     # flags
@@ -47,31 +50,27 @@ if __name__ == "__main__":
             sleep(1)
             print("The crowd throws tomatoes at you.")
             sleep(1)
-            print("GAME OVER")
+            print("GAME OVER\n")
             exit()
         else:
-            print("Hm? Speak up son.")
+            print("Hm? Speak up son.\n")
 
     thread = threading.Thread(target=ai.punch, args=[guy])
     thread.daemon = True
     thread.start()
 
     while fight:
-        if guy.hp <= 0:
-            print("You fall to the ground.")
-            sleep(1)
-            print("GAME OVER")
-            exit()
+        x = input()
+        if x == "help":
+            print("Your opponent will attack until you're on the ground.")
+            print("Your moves:")
+            print(" - Right hook: `r hook`")
+            print(" - Left hook: `l hook`")
+            print(" - Right jab: `r jab`")
+            print(" - Left jab: `l jab`")
+            print(" - Uppercut: `uppercut`")
+            print(" - Display your hp: `hp`\n")
+        elif x == "hp":
+            print("Your hp: ", guy.hp,"\n")
         else:
-            x = input("> ")
-            if x == "help":
-                print("Your opponent will attack until you're on the ground.")
-                print("Your moves:")
-                print(" - Right hook: `r hook`")
-                print(" - Left hook: `l hook`")
-                print(" - Right jab: `r jab`")
-                print(" - Left jab: `l jab`")
-                print(" - Uppercut: `uppercut`")
-            else:
-                print("What?")
-        print("")
+            print("What?\n")
